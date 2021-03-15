e84f9b902d49b9807485cc65448660064bcdc422
yalemsew mekuriaw <yalemone@gmail.com>
2021-03-15 14:47:12 +0530

    adding

---
+++ 000Dev/001consolePromptAlert.js
@@ -0,0 +1,8 @@
+var firstUserName=prompt("what is your first name?");
+var lastUserName=prompt("what is your last name?");
+var age=prompt("what is your age?");
+console.log(firstUserName);
+console.log(lastUserName);
+console.log(age);
+
+
